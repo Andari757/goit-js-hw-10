12e560044f2f2702bcfbc95b26177cadb572bdf7
Andari757 <matviyskrypka@gmail.com>
2022-04-25 20:58:04 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import './css/styles.css';
 import debounce from "lodash.debounce"
 const DEBOUNCE_DELAY = 300;
+const ul = document.querySelector(".country-list")
 const input = document.querySelector("#search-box")
 input.addEventListener("input", debounce(() => {
     fetchCountries(input.value)
 },DEBOUNCE_DELAY))
 function fetchCountries(value) {    
-    fetch(`https://restcountries.com/v3.1/name/${input.value}`)
-        .then(r => { return r.json() })
-        .then(r => { console.log(r) })
+    fetch(`https://restcountries.com/v3.1/name/${input.value}?fields=flags,name,capital,population,languages `)
+        
+        .then(r => {            
+            return r.json()
+        })
+        .then(r => {
+            console.log(r)
+            r.forEach(e => {
+               console.log(Object.values(e.languages))
+                ul.insertAdjacentHTML("afterbegin", `<li>${e.name.official}${e.capital}${e.population}${e.languages}</li>`)
+            });
+            
+            
+        })
         .catch(er=>{ console.log(er)})
 }
