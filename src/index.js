d1a48892afa50b7a74116ec70aaa59bec0e4fd68
Andari757 <matviyskrypka@gmail.com>
2022-04-25 19:55:14 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import './css/styles.css';
-
+import debounce from "lodash.debounce"
 const DEBOUNCE_DELAY = 300;
 const input = document.querySelector("#search-box")
-input.addEventListener("input",fetchCountries(input.value))
+input.addEventListener("input", debounce(() => {
+    fetchCountries(input.value)
+},DEBOUNCE_DELAY))
 function fetchCountries(value) {    
     fetch(`https://restcountries.com/v3.1/name/${input.value}`)
         .then(r => { return r.json() })
