5c5010c75119326529e5752a348601c4de7d6f1b
Andari757 <matviyskrypka@gmail.com>
2022-04-25 18:05:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
 const input = document.querySelector("#search-box")
-console.log(input)
+input.addEventListener("input",fetchCountries(input.value))
+function fetchCountries(value) {    
+    fetch(`https://restcountries.com/v3.1/name/${input.value}`)
+        .then(r => { return r.json() })
+        .then(r => { console.log(r) })
+        .catch(er=>{ console.log(er)})
+}
