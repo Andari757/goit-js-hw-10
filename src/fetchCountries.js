fd2f1b18010da3aa2c4bc5950b91c27ab1084f80
Andari757 <matviyskrypka@gmail.com>
2022-04-27 14:51:04 +0300

    1

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,42 +1,10 @@
-const ul = document.querySelector(".country-list")
-import Notiflix from 'notiflix';
-export function fetchCountries(value) {    
-    value = value.trim();
-    if (!value) {  
-        clear()
-        return
-    }        
-    fetch(`https://restcountries.com/v3.1/name/${value}?fields=flags,name,capital,population,languages `)
+const baseUrl = 'https://restcountries.com/v3.1/name/'
+export function fetchCountries(value) {   
+    return fetch(`${baseUrl}${value}?fields=flags,name,capital,population,languages `)
         .then(r => {            
             if (!r.ok) {
                 throw new Error(r.status)
             }
             return r.json()
-        })
-        .then(r => {
-            showResult(r)
-        })
-        .catch(er => {
-        clear()
-        console.log(er)
-        Notiflix.Notify.failure('Oops, there is no country with that name')
-                })        
+        })              
 }
-function clear() {
-    ul.innerHTML = ""
-    ul.nextElementSibling.innerHTML = ""
-            }
-function showResult(r) {              
-            clear()
-            if (r.length > 10) {
-                Notiflix.Notify.info('Too many matches found. Please enter a more specific name');
-                return
-            }else if (r.length<=10 && r.length>=2) {                
-            r.forEach(e => {
-                ul.insertAdjacentHTML("afterbegin", `<li><img width="50" src=${e.flags.svg}>${e.name.official}</li>`)
-                return
-            })     
-            } else {
-                ul.nextElementSibling.insertAdjacentHTML("afterbegin", `<img width="50" src=${r[0].flags.svg}> ${r[0].name.official}<br> capital: ${r[0].capital}<br> population: ${r[0].population}<br> languages: ${Object.values(r[0].languages)}`)
-            }            
-        }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,47 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 import debounce from "lodash.debounce"
+import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
-
+const ul = document.querySelector(".country-list")
+const failureText = 'Oops, there is no country with that name'
+const infoText = 'Too many matches found. Please enter a more specific name'
 const input = document.querySelector("#search-box")
 input.addEventListener("input", debounce(() => {
-    fetchCountries(input.value)
-},DEBOUNCE_DELAY))
+    if (!input.value) {  
+        clear()
+        return
+    }    
+    fetchCountries(input.value.trim())        
+    .then(r => {
+            showResult(r)
+        })
+    .catch(er => {
+        clear()
+        console.log(er)
+        Notiflix.Notify.failure(failureText)
+        })     
+}, DEBOUNCE_DELAY))
+function showResult(r) {
+    clear()
+    if (r.length > 10) {
+        Notiflix.Notify.info(infoText);
+        return
+    } else if (r.length <= 10 && r.length >= 2) {
+        renderLi(r)
+        return
+    } else {
+        renderDiv(r)
+    }
+}
+function clear() {
+    ul.innerHTML = ""
+    ul.nextElementSibling.innerHTML = ""
+}
+function renderLi(r) {
+    const markup = r.map((e) =>`<li><img width="50" src=${e.flags.svg}>${e.name.official}</li>`) .join("")
+        ul.insertAdjacentHTML("afterbegin", markup)
+}
+function renderDiv(r) {
+    ul.nextElementSibling.insertAdjacentHTML("afterbegin", `<img width="50" src=${r[0].flags.svg}> ${r[0].name.official}<br> capital: ${r[0].capital}<br> population: ${r[0].population}<br> languages: ${Object.values(r[0].languages)}`)
+}
